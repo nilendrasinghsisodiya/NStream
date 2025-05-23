a8294deba53d42aa45eb9450237d68456f1bb19e
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-05-23 13:46:57 +0530

    adding a healthcheckfile

---
+++ src/routes/healthCheck.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controller.js";
+
+const hcRouter = Router()
+
+hcRouter.get('/health',healthcheck);
+export default hcRouter;
