f1ec826da48ed96f7da21c49b023f5dbd32b59c0
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-07-11 18:32:15 +0530

    fix: added patch request method in cros origin

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(
   cors({
    origin: 'http://localhost:5173', // ✅ Specific origin, NOT '*'
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization','optional'],
   })
 );
