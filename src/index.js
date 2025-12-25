ad45496bd1885251b8d42d1446a8183e51e5d774
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2025-12-25 16:27:22 +0530

    fix: centralized approch to intialized server, database , redis and bullmq worker

--- src/index.js
+++ src/index.js
@@ -1,75 +1,48 @@
-import dotenv from "dotenv";
-import path from "path";
-import { __dirname } from "./fileUtils.js";
-import { updateManyFieldsInDoc } from "./utils/additionalUtils.js";
+import dotenv from 'dotenv';
+import path from 'path';
+import { __dirname } from './utils/fileUtils.js';
+//import { updateManyFieldsInDoc } from './utils/additionalUtils.js'; // only for dev
 
-const env_file_path = path.resolve(__dirname, ".env");
-
+const env_file_path = path.resolve(__dirname, '../.env');
 console.log(env_file_path);
 dotenv.config({ path: env_file_path });
-import connectDB from "./db/connectDB.js";
-import { app } from "./app.js";
-import { Video } from "./models/video.model.js";
-import { User } from "./models/user.model.js";
-import { deleteFromCloudinary } from "./utils/cloudinary.js";
-import { otpQueue } from "./messageQueue/bullmq.setup.js";
+import connectDB from './db/connectDB.js';
+import { app } from './app.js';
+import { initRedis } from './redis/redis.setup.js';
+import { initOtpWorker } from './worker/otp.worker.js';
+const init = async () => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error('DB CONNECTION:: failed', err.message);
 
-connectDB()
-  .then(() => {
-    try {
-      app.on("error", (error) => {
-        console.log("Application ERROR : ", error);
-        throw error;
-      });
-      // remove after done one time use
+    return;
+  }
 
-      app.listen(process.env.PORT || 8000, () => {
-        console.log(`Sever listening on port: ${process.env.PORT}`);
+  try {
+    await initRedis();
+  } catch (err) {
+    console.error('REDIS INIT FAILED:: ', err.message);
+  }
 
-        // (async () => {
-        //   User.find({ coverImagePublicId: { $nin: [null, ""] } }).then(
-        //     (users) => {
-        //       console.log("users that have a cover images", users);
-        //       const usersPlain = users.map((u) => u.toObject());
-
-        //       usersPlain.forEach((u) => {
-        //         console.log(u.coverImagePublicId);
-        //         deleteFromCloudinary(u.coverImagePublicId).then(() =>
-        //           console.log("cover Image deleted form user ", u.username)
-        //         );
-        //       });
-        //     }
-        //   );
-        // })();
-      });
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  })
-  .catch((err) => {
-    console.log("MONGODB connection failed !!!");
+  try {
+    await initOtpWorker();
+  } catch (err) {
+    console.error('OTP WORKER INIT FAILED:: ', err.message);
+  }
+  app.on('error', (error) => {
+    console.error('APPLICATION ERROR ::', error);
+    throw error;
   });
 
-// updateManyFieldsInDoc(Video,{},{$set:{deleted:false}});
-otpQueue.add("opt",{useremail:"okashishow@gmail",otp:123456});
-/*(async ()=>{
-    try {
-      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-      app.on("error",(error)=>{
-        console.log("ERROR: ", error)
-        throw error
-      });
-      app.listen(process.env.PORT, ()=>{
-        console.log(`app is listening on port ${process.env.PORT}`)
-      })
+  app.listen(process.env.PORT, () => {
+    console.log(`SERVER LISTENING ON PORT: ${process.env.PORT}`);
+  });
+};
 
-      
-    } catch (error) {
-        console.error("ERROR: ",error)
-        throw error
-    }
-})();
-
-APPROCH 1 
-*/
+try {
+  init();
+} catch (error) {
+  console.error(error);
+  throw error;
+}
