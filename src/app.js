831874a043d66e023aa40c648c768f9b6f9f8dda
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2025-12-25 16:47:29 +0530

    fix: sperated controller for deleteUser because of using emailed token for auth

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import path from "path";
-import helmet from "helmet";
-import fs from "fs";
-import https from "https";
-import { __dirname } from "./utils/fileUtils.js";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+//import path from 'path';
+import helmet from 'helmet';
+//import fs from "fs";
+//import https from "https";
+//import { __dirname } from "./utils/fileUtils.js";
 
 const app = express();
 // const key = fs.readFileSync(
@@ -19,60 +19,60 @@ app.use(helmet());
 // leaving at * because of issue in development because of self signed certs
 app.use(
   cors({
-    //origin: 'http://localhost:5173',// ✅ Specific origin, NOT '*'
     credentials: true,
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "optional"],
-  })
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'optional'],
+  }),
 );
 
 app.use(
   express.json({
-    limit: "16kb",
-  })
+    limit: '16kb',
+  }),
 );
 app.use(
   express.urlencoded({
     extended: true,
-    limit: "16kb",
-  })
+    limit: '16kb',
+  }),
 );
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // routes import
-import { userRouter } from "./routes/user.routes.js";
-import { likeRouter } from "./routes/like.routes.js";
-import { videoRouter } from "./routes/video.routes.js";
-import { commentRouter } from "./routes/comment.routes.js";
-import { dashboardRouter } from "./routes/dashboard.routes.js";
-import { hcRouter } from "./routes/healthCheck.routes.js";
-import { playlistRouter } from "./routes/playlist.routes.js";
-import { authRouter } from "./routes/auth.routes.js";
+import { userRouter } from './routes/user.routes.js';
+import { likeRouter } from './routes/like.routes.js';
+import { videoRouter } from './routes/video.routes.js';
+import { commentRouter } from './routes/comment.routes.js';
+import { dashboardRouter } from './routes/dashboard.routes.js';
+import { hcRouter } from './routes/healthCheck.routes.js';
+import { playlistRouter } from './routes/playlist.routes.js';
+import { authRouter } from './routes/auth.routes.js';
+import { tokenedRouter } from './routes/tokened.routes.js';
 // routes declaration
 
-app.use("/api/v1/user", userRouter);
+app.use('/api/v1/user', userRouter);
 
-app.use("/api/v1/auth", authRouter);
+app.use('/api/v1/tokened', tokenedRouter);
 
-app.use("/api/v1/like/", likeRouter);
+app.use('/api/v1/auth', authRouter);
 
-app.use("/api/v1/video", videoRouter);
+app.use('/api/v1/like/', likeRouter);
 
-app.use("/api/v1/playlist", playlistRouter);
+app.use('/api/v1/video', videoRouter);
 
-app.use("/api/v1/comment", commentRouter);
+app.use('/api/v1/playlist', playlistRouter);
 
-app.use("/api/v1/dashboard", dashboardRouter);
+app.use('/api/v1/comment', commentRouter);
 
-app.use("/healthCheck", hcRouter);
+app.use('/api/v1/dashboard', dashboardRouter);
 
-// app.use("/api/v1/tweet", tweetRouter);
-app.get("/check", (req, res) => {
-  res.send("hello world");
+app.use('/healthCheck', hcRouter);
+
+app.get('/check', (req, res) => {
+  res.send('hello world');
 });
-app.use(express.static(path.join(__dirname, "dist")));
 
 app.use((err, req, res, next) => {
   console.error(err); // Log error to console
@@ -80,11 +80,8 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     success: false,
     status: err.statusCode,
-    message: err.message || "Internal Server Error",
+    message: err.message || 'Internal Server Error',
   });
 });
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.join(__dirname,  "dist", "index.html"))
-// })
 
 export { app };
