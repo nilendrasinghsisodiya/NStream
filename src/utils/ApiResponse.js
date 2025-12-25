1cf92485bf20fd09d96b66fbcccde86d51ac5966
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2025-12-25 16:17:03 +0530

    format: just some formatting change

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-  constructor(statusCode, data, message = "Success") {
+  constructor(statusCode, data, message = 'Success') {
     this.data = data;
     this.message = message;
     this.statusCode = statusCode;
@@ -8,4 +8,3 @@ class ApiResponse {
 }
 
 export { ApiResponse };
-
