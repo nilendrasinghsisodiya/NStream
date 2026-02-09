fc1a979462907b9ebe73f208241dfc73978d1489
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-10 00:38:02 +0530

    fix: fixed typo in mime types

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,14 @@
-export const allowedImageType = [
-  "image/png",
-  "image/jpg",
-  "image/webp",
-  "image/jepg",
-];
+export const allowedImageType = ['image/png', 'image/jpg', 'image/webp', 'image/jpeg'];
 
 export const allowedVideoType = [
-  "video/mp4",
-  "video/x-msvideo",
-  "video/mpeg",
-  "video/avi",
-  "video/avi",
+  'video/mp4',
+  'video/x-msvideo',
+  'video/mpeg',
+  'video/avi',
+  'video/avi',
 ];
 
+export const LOCK_TIMEOUT = 15 * 60 * 1000;
 
-export const sortType = ["asc","desc"];
+export const sortType = ['asc', 'desc'];
 export const sortBy = [];
