927845f19960a12a7001bd68fe6ec9fd7d8d0ba8
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-14 23:15:05 +0530

    fix: made init singleton

--- src/messageQueue/bullmq.setup.js
+++ src/messageQueue/bullmq.setup.js
@@ -1,11 +1,21 @@
 // At the very top of each file that runs in its own process
 import { Queue } from 'bullmq';
 import { getRedis } from '../redis/redis.setup.js';
-export let otpQueue = undefined;
+let otpQueue = undefined;
 
-const redis = await getRedis();
-try {
-  otpQueue = new Queue('otpQueue', { connection: redis });
-} catch (err) {
-  console.log('messagequeue', err.message);
-}
+const initOtpQueue = async () => {
+  if (otpQueue) return otpQueue;
+  const redis = await getRedis();
+  try {
+    otpQueue = new Queue('otpQueue', { connection: redis });
+  } catch (err) {
+    console.error('messagequeue', err.message);
+  }
+};
+
+const getOtpQueue = () => {
+  if (!otpQueue) throw new Error('otp queue not initialized');
+  return otpQueue;
+};
+
+export { getOtpQueue, initOtpQueue };
