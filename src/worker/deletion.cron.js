7291e49a0013f20e6c0b08a7a18a8486c00590d6
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-14 23:16:09 +0530

    moved fileUptils.js to utils folder, and added cron worker for db cleanup , and message queue worker for processing otp queue

--- src/worker/deletion.cron.js
+++ src/worker/deletion.cron.js
@@ -1,13 +1,14 @@
-import nodeCron from "node-cron";
-import { User } from "../models/user.model.js";
-import { Video } from "../models/video.model.js";
-import { Comment } from "../models/comment.model.js";
-import { Playlist } from "../models/playlist.model.js";
-import { PresistView } from "../models/view.model.js";
-import { Like } from "../models/like.model.js";
-import { Subscription } from "../models/subscription.model.js";
-import mongoose from "mongoose";
-
+import nodeCron from 'node-cron';
+import { User } from '../models/user.model.js';
+import { Video } from '../models/video.model.js';
+import { Comment } from '../models/comment.model.js';
+import { Playlist } from '../models/playlist.model.js';
+import { PresistView } from '../models/view.model.js';
+import { Like } from '../models/like.model.js';
+import { Subscription } from '../models/subscription.model.js';
+import mongoose from 'mongoose';
+import { LOCK_TIMEOUT } from '../constants.js';
+import { deleteFromCloudinary } from '../utils/cloudinary.js';
 /* strategy -> if we are deleting user then
 delete PresistViews -> likes -> subscriptions -> comments ->playlists -> videos -> delete its video from cloudinary, thumbnail form cloudinary -> delete user avatar from cloundinary->then atlast delete the user */
 
@@ -17,18 +18,27 @@ delete PresistViews -> likes -> subscriptions -> comments ->playlists -> videos 
 // playlist it is direct
 // on avatar change delete form cloudinary sync or just create a new doc for it then delete mark it as delete (soft delete ) then delete it when the cron-job is runing*/
 
-const deleteVideo = async (videoId, session) => {
+const deleteVideo = async (mv, session) => {
+  const videoId = mv._id;
   try {
-    await session.startTransaction();
+    session.startTransaction();
     await Comment.deleteMany({ video: videoId }).session(session);
     await Like.deleteMany({ targetId: videoId }).session(session);
     await PresistView.deleteMany({ targetId: videoId }).session(session);
     // Cloudinary deletion is external → cannot be in transaction
-    await deleteFromCloudinary(videoId);
-    await Video.deleteOne({ _id: videoId }).session(session);
+
+    await Video.findByIdAndDelete(videoId).session(session);
     await session.commitTransaction();
+    await deleteFromCloudinary(mv.thumbnailPublicId);
+    await deleteFromCloudinary(mv.videoPublicId);
   } catch (err) {
+    await Video.findByIdAndUpdate(videoId, {
+      $inc: {
+        deletionTries: 1,
+      },
+    });
     console.log(`500::CRON:: failed to delete video ${videoId}`, err.message);
+
     await session.abortTransaction();
   } finally {
     await session.endSession();
@@ -36,38 +46,49 @@ const deleteVideo = async (videoId, session) => {
 };
 
 const deleteMarkedVideos = async () => {
-  const markedVideos = await Video.find({ deleted: true });
-  markedVideos.forEach(async (mv) => {
-    const session = await mongoose.StartSession();
-    deleteVideo(mv._id, session);
-  });
-};
+  while (true) {
+    const now = new Date();
 
+    const video = await Video.findOneAndUpdate(
+      {
+        deleted: true,
+        deletionTries: { $lt: 5 },
+        $or: [{ deleteLock: { $ne: true } }, { lockAt: { $lt: new Date(now - LOCK_TIMEOUT) } }],
+      },
+      {
+        $set: { deleteLock: true, lockAt: now },
+      },
+      { new: true },
+    );
+
+    if (!video) break; // nothing left to process
+
+    const session = await mongoose.startSession();
+
+    try {
+      await deleteVideo(video, session);
+    } catch (err) {
+      // release lock on failure
+      await Video.findByIdAndUpdate(video._id, {
+        $set: { deleteLock: false },
+      });
+    }
+  }
+};
 const deleteUser = async (userId, session) => {
   try {
-    await session.startTransaction();
+    session.startTransaction();
     await Comment.deleteMany({ owner: userId }).session(session);
     await Playlist.deleteMany({ owner: userId }).session(session);
-    const videos = await Video.find({ owner: userId });
-    try {
-      videos.forEach(async (v) => {
-        const newSession = await mongoose.startSession();
-        deleteVideo(v._id, newSession);
-      });
-    } catch (err) {
-      console.error(
-        `500::CRON:: some video deletion failed for user ${userId} aborting session`,
-        err.message
-      );
-      await session.abortTransaction();
-      throw new Error(err.message);
-    }
+
+    await Video.updateMany({ owner: userId }, { $set: { deleted: true } }).session(session);
     await Subscription.deleteMany({ subscriber: userId }).session(session);
     await Subscription.deleteMany({ channel: userId }).session(session);
-    await User.findById(userId).session(session);
+    await User.findByIdAndDelete(userId).session(session);
     await session.commitTransaction();
   } catch (error) {
     console.log(`500::CRON:: failed to delete user ${userId}`, error.message);
+    await session.abortTransaction();
   } finally {
     await session.endSession();
   }
@@ -75,14 +96,15 @@ const deleteUser = async (userId, session) => {
 
 const deleteMarkedUser = async () => {
   const markedUser = await User.find({ deleted: true });
-  markedUser.forEach(async (mu) => {
+  for (const mu of markedUser) {
     const session = await mongoose.startSession();
     await deleteUser(mu._id, session);
-  });
+  }
 };
 
-nodeCron.schedule("0 0 0 * * *", async () => {
+nodeCron.schedule('0 0 0 * * *', async () => {
   try {
+    await mongoose.connect(process.env.MONGODB_URI);
     await deleteMarkedUser();
     await deleteMarkedVideos();
   } catch (error) {
--- src/worker/otp.worker.js
+++ src/worker/otp.worker.js
@@ -1,83 +1,74 @@
 // At the very top of each file that runs in its own process
-import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.resolve("test.env") });
+import { Worker } from 'bullmq';
+import { getRedis } from '../redis/redis.setup.js';
+import { otpMailTemplate, actionEmailTemplate, sendMail } from '../utils/mailUtils.js';
 
+const initOtpWorker = async () => {
+  const redis = await getRedis();
 
-import { Worker } from "bullmq";
-import { redis } from "../redis/redis.setup.js";
-import nodemailer from "nodemailer";
-console.log("type","OAUTH2",
-    "user", process.env.APP_MAIL,
-    "clientId", process.env.GOOGLE_CLIENT_ID,
-    "clientSecret", process.env.GOOGLE_CLIENT_SECRET,
-    "refreshToken", process.env.GOOGLE_REFRESH_TOKEN,
-  )
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type:"OAUTH2",
-    user: process.env.APP_MAIL,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
-});
+  const otpWorker = new Worker(
+    'otpQueue',
+    async (job) => {
+      try {
+        switch (job.name) {
+          case 'otp': {
+            const otpJobData = job.data;
+            await sendMail(
+              otpMailTemplate({ to: otpJobData.usermail, otp: otpJobData.otp, expiryMinutes: 10 }),
+            );
+            break;
+          }
+          case 'otToken': {
+            const tokenJobData = job.data;
+            if (!['verifyEmail', 'delete'].includes(tokenJobData.action)) {
+              throw new Error(`Unknow token action: ${tokenJobData.action}`);
+            }
+            await sendMail(
+              actionEmailTemplate({
+                to: tokenJobData.usermail,
+                userName: tokenJobData.username,
+                actionName: tokenJobData.action,
+                expiryMinutes: 60 * 30,
+                actionLink: `${process.env.DOMAIN}/action=${tokenJobData.action}&otToken?token=${tokenJobData.token}`,
+              }),
+            );
+            break;
+          }
+          default: {
+            throw new Error(`Unkown job: ${job.name}`);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    {
+      connection: redis,
+      removeOnComplete: {
+        age: 300,
+        count: 0,
+      },
+      removeOnFail: {
+        count: 100,
+      },
 
-const sendmail = async (email, otp) => {
-  try {
-    console.log("in sendmail");
-    const info = await transporter.sendMail({
-      from: process.env.APP_MAIL,
-      to: email,
-      subject: "OTP Verification For NStream",
-      text: `Your otp is : ${otp}`,
-    });
-    console.log("otp sent", info.messageId);
-  } catch (error) {
-    console.error(error.message);
-  }
+      autorun: false,
+      concurrency: 50,
+    },
+  );
+  otpWorker.on('connection', () => {
+    console.log('worker has connected sucessfully');
+  });
+  otpWorker.on('drained', () => {
+    console.log('all jobs have been completed');
+  });
+
+  otpWorker.on('failed', (job) => {
+    console.log(`Error:: failed to compelete \n jobId: ${job.id} \t jobData: ${job.data} \n`);
+  });
+
+  return otpWorker;
 };
 
-const otpWorker = new Worker(
-  "otpQueue",
-  async (job) => {
-    try {
-      const { usermail, otp } = job.data;
-      console.log(usermail, otp);
-      await sendmail(usermail, otp);
-
-      console.log("userId", usermail, "otp", otp);
-    } catch (error) {
-      console.log(error.message);
-    }
-  },
-  {
-    connection: redis,
-    removeOnComplete: {
-      age: 300,
-      count: 0,
-    },
-    removeOnFail: {
-      count: 100,
-    },
-
-    autorun:false,
-    concurrency: 50,
-  }
-);
-otpWorker.on("connection", () => {
-  console.log("worker has connected sucessfully");
-});
-otpWorker.on("drained", () => {
-  console.log("all jobs have been completed");
-});
-
-otpWorker.on("failed", (job) => {
-  console.log(
-    `Error:: failed to compelete \n jobId: ${job.id} \t jobData: ${job.data} \n`
-  );
-});
-
-export { otpWorker };
+export { initOtpWorker };
