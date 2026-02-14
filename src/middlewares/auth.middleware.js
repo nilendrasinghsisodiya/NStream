0d003ee5812d298b14627a7017a632432f5937a6
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-14 23:12:01 +0530

    chore:formatting changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,41 +1,35 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model.js';
 export const verifyJwt = asyncHandler(async (req, res, next) => {
   try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
-    const isOptional = req.header("Optional");
-    if (!token && isOptional === "true") {
+    const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+    const isOptional = req.header('Optional');
+    if (!token && isOptional === 'true') {
       req.user = null;
+      req.optional = true;
       next();
       return;
     }
 
     if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+      throw new ApiError(401, 'Unauthorized request');
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    const user = await User.findById(decodedToken?._id).select(
-      "-password -refreshToken"
-    );
+    const user = await User.findById(decodedToken?._id).select('-password -refreshToken');
     if (!user) {
-      throw new ApiError(401, "Invalid Access Token");
+      throw new ApiError(401, 'Invalid Access Token');
     }
     req.user = user;
     return next();
   } catch (error) {
-    if (error.message === "jwt expired") {
-      throw new ApiError(
-        493,
-        "accessToken expired, please relogin or refresh the token"
-      );
+    if (error.message === 'jwt expired') {
+      throw new ApiError(493, 'accessToken expired, please relogin or refresh the token');
     } else {
-      throw new ApiError(401, error.message || "invalid accessToken");
+      throw new ApiError(401, error.message || 'invalid accessToken');
     }
   }
 });
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,24 +1,20 @@
-import multer from "multer"
-import path from "path"
- // multer give two options disk storage and memory (ram) storage we should avoid memory storage without limits
+import multer from 'multer';
+import path from 'path';
+// multer give two options disk storage and memory (ram) storage we should avoid memory storage without limits
 
 const storage = multer.diskStorage({
-    // sets where file is saved
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    // set filename unique filename
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname) )
-    }
-  })
-  
+  // sets where file is saved
+  destination: function (req, file, cb) {
+    cb(null, './public/temp');
+  },
+  // set filename unique filename
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
 
-  // set options to multer and export it
-   export const upload = multer({ 
-    storage: storage
- })
-
-
-
+// set options to multer and export it
+export const upload = multer({
+  storage: storage,
+});
