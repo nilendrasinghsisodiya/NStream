2d403decc3b136efdd29ff1bbe99c5d865a11bb9
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-14 23:14:40 +0530

    fix: made initRedis, and get Redis singleton

--- src/redis/redis.setup.js
+++ src/redis/redis.setup.js
@@ -1,17 +1,22 @@
 // At the very top of each file that runs in its own process
 import { Redis } from 'ioredis';
-let redis = undefined;
+let redis = null;
 export async function initRedis() {
   if (redis) return redis;
-
-  redis = new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null, enableReadyCheck: false });
-  redis.on('error', (error) => {
-    console.error('REDIS ERROR:: ', error);
-  });
-  await redis.ping(); // fail early if needed
-  console.log('Redis connected');
-
-  return redis;
+  try {
+    redis = new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+      enableReadyCheck: false,
+    });
+    redis.on('error', (error) => {
+      console.error('REDIS ERROR:: ', error);
+    });
+    await redis.ping(); // fail early if needed
+    console.log('Redis connected');
+    return redis;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const getRedis = () => {
